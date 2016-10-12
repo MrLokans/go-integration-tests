c6fdc03eac368d9f9c66232275f11445c1528264
MrLokans <trikster1911@gmail.com>
2016-10-12 16:26:57 +0300

    Main server interacting with auth service added.

--- main.go
+++ main.go
@@ -2,16 +2,59 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	// "net/http"
+	"net/http"
 )
 
+var auth = authService{Base: "http://localhost:4546"}
+
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
-	router.GET("/user/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.JSON(200, gin.H{"user": name, "status": "ok"})
-	})
-
+	router.POST("/login", loginFunc)
+	router.GET("/logout", logoutFunc)
+	router.POST("/protected-content", serveProtectedFunc)
 	router.Run(":4545")
+}
+
+func loginFunc(c *gin.Context) {
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	if response := auth.Login(username, password); response.Token != "" {
+		c.SetCookie("username", username, 3600, "", "", false, true)
+		c.SetCookie("token", response.Token, 3600, "", "", false, true)
+
+		c.JSON(http.StatusOK, response)
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+}
+
+func logoutFunc(c *gin.Context) {
+	username, err1 := c.Cookie("username")
+	token, err2 := c.Cookie("token")
+
+	if err1 == nil && err2 == nil && auth.Logout(username, token) {
+		c.SetCookie("username", "", -1, "", "", false, true)
+		c.SetCookie("token", "", -1, "", "", false, true)
+
+		c.JSON(http.StatusOK, nil)
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+}
+
+func serveProtectedFunc(c *gin.Context) {
+	username, err1 := c.Cookie("username")
+	token, err2 := c.Cookie("token")
+
+	if err1 == nil && err2 == nil && auth.Authenticate(username, token) {
+		c.SetCookie("username", "", -1, "", "", false, true)
+		c.SetCookie("token", "", -1, "", "", false, true)
+
+		c.JSON(http.StatusOK, gin.H{"message": "You are successfully authorized"})
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
 }
